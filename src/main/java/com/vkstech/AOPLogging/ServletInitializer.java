e01f663f67a85ed0fa288f593938072115ebbd30
vipul-kumar-singh <vipul24.9669@gmail.com>
2021-04-06 23:15:01 +0530

    Initial Commit

---
+++ src/main/java/com/vkstech/AOPLogging/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.vkstech.AOPLogging;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(AopLoggingApplication.class);
+	}
+
+}
---
+++ src/test/java/com/vkstech/AOPLogging/AopLoggingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vkstech.AOPLogging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AopLoggingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
